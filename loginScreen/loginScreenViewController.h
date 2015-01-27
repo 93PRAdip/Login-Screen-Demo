4f7651b1284e4e171aa5df5961fe2c41ee05c254
pradip <pradip@pradips-Mac.local>
2015-01-27 11:03:49 +0530

    first commite

---
+++ loginScreen/loginScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  loginScreenViewController.h
+//  loginScreen
+//
+//  Created by pradip on 1/27/15.
+//  Copyright (c) 2015 pradip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface loginScreenViewController : UIViewController
+
+@end
